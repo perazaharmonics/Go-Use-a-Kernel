661a0f992cd63583532703f532ee3813123f5ae7
Enrique Peraza <141517235+perazaharmonics@users.noreply.github.com>
2025-05-08 08:31:25 -0400

    Cleaner version
    Added a mutex for goroutine-local locks and System V semaphores for IPC locking and unlocking to perform sage interprocess operations on a shared resource.

--- logger/logger.go
+++ logger/logger.go
@@ -1,13 +1,8 @@
-/**********************************************************************
-* Filename:
-*  logger.go
-* Description:
-*   A wrapper function for the Go log package, such that it records
-*   the timestamp, the file name, function name and if it's an error
-*   the error code, error symbol and the error message.
-* Author:
-*  JEP J. Enrique Peraza
-*********************************************************************/
+/*
+* A wrapper function for the Go log package, such that it records
+* the timestamp, the file name, function name and if it's an error
+* the error code, error symbol and the error message.
+*/
 
 package logger
 
@@ -327,10 +322,8 @@ func (l *Logger) writeToFile(file,msg string) {
 // to the specified text file.
 func (l *Logger) logMessage(level LogLevel, msg string) {
     if level < l.Level {                // Log level less than current level?
-    return                              // If so, return without logging.
-    }                                   // Otherwise, continue.
-    sem.Lock("Because we are writing to the text file.")
-    defer sem.Unlock("Because we are done writing to the text file.")
+		return                              // If so, return without logging.
+	}                                     // Otherwise, continue.
     switch level {                      // Set the symbol based on the log level
     case Debug:                         // Debug level?
       l.Symbol = "[DEBUG] "             // Set symbol to [DEBUG]
@@ -341,30 +334,8 @@ func (l *Logger) logMessage(level LogLevel, msg string) {
     case Error:                         // Error level?
       l.Symbol = "! "                   // Set symbol to !
     case Fatal:                         // Fatal level?
-      l.Symbol = "@ "                  // Set symbol to !!
+      l.Symbol = "@ "                   // Set symbol to !!
     }                                   // Done setting the symbol
-    // -------------------------------- //
-    // Get the file size to check if it exceeds 500KiB,
-    // if so, clear the log file.
-    // -------------------------------- //
-    flogInfo, err := os.Stat(logpathname)// Get the file info
-    if err != nil {                     // Error getting file info?
-      fmt.Printf("Failed to get file info: %v\n", err)// Error getting file info
-      return                            // Return if error
-    }                                   // Otherwise, continue.
-    ferrInfo, ers := os.Stat(errpathname)// Get the error file info
-    if ers != nil {                     // Error getting file info?
-      fmt.Printf("Failed to get file info: %v\n", ers)// Error getting file info
-      return                            // Return if error
-    }                                   // Otherwise, continue.
-    flogSiz:=flogInfo.Size()            // Get the file size
-    if flogSiz > maxLogSize {           // If file size exceeds 30KB
-      l.clearLogFile(logpathname)       // Clear the log file
-    }                                   // Otherwise, continue.
-    ferrSiz:=ferrInfo.Size()            // Get the error file size
-    if ferrSiz > maxLogSize {           // If file size exceeds 30KB
-      l.clearLogFile(errpathname)       // Clear the error file
-    }                                   // Otherwise, continue.
   // ---------------------------------- //
   // If the message in the buffer is a multiline message we will purge that
   // buffer and set a recursive entrypoint so that it enters the log
@@ -380,6 +351,40 @@ func (l *Logger) logMessage(level LogLevel, msg string) {
     }                                   // Done splitting the message.
     return                              // Return if we had to purge a message.
   }                                     // Otherwise no newline so just fall through.
+  // ---------------------------------- //
+  // Lock the mutex so that you have a goroutine-local lock
+  // and unlock it when done.
+  // ---------------------------------- //
+  l.mu.Lock()                           // Lock the mutex to protect the log file
+  defer l.mu.Unlock()                   // Unlock the mutex after writing
+  // ---------------------------------- //
+  // Lock the semaphore to ensure only one process can write to the file at a time
+  // and unlock it when done.
+  // ---------------------------------- //
+  sem.Lock("Because we are writing to the text file.")
+  defer sem.Unlock("Because we are done writing to the text file.")
+  // -------------------------------- //
+  // Get the file size to check if it exceeds 500KiB,
+  // if so, clear the log file.
+  // -------------------------------- //
+  flogInfo, err := os.Stat(logpathname)// Get the file info
+  if err != nil {                     // Error getting file info?
+    fmt.Printf("Failed to get file info: %v\n", err)// Error getting file info
+    return                            // Return if error
+  }                                   // Otherwise, continue.
+  ferrInfo, ers := os.Stat(errpathname)// Get the error file info
+  if ers != nil {                     // Error getting file info?
+    fmt.Printf("Failed to get file info: %v\n", ers)// Error getting file info
+    return                            // Return if error
+  }                                   // Otherwise, continue.
+  flogSiz:=flogInfo.Size()            // Get the file size
+  if flogSiz > maxLogSize {           // If file size exceeds 30KB
+    l.clearLogFile(logpathname)       // Clear the log file
+  }                                   // Otherwise, continue.
+  ferrSiz:=ferrInfo.Size()            // Get the error file size
+  if ferrSiz > maxLogSize {           // If file size exceeds 30KB
+    l.clearLogFile(errpathname)       // Clear the error file
+  }                                   // Otherwise, continue.
   // ---------------------------------- //
 	// Write the log message to the file
 	// ---------------------------------- //
@@ -429,122 +434,6 @@ func (l *Logger) logMessage(level LogLevel, msg string) {
     }                                   // Done with long line. 
   }                                     // Done with while we have to write.
 }                                       // ---------logMessage-------- //
-/*
-func (l *Logger) logMessage(level LogLevel, msg string) {
-    if level < l.Level {                // Log level less than current level?
-		return                              // If so, return without logging.
-	}                                     // Otherwise, continue.
-	l.mu.Lock()                           // Lock mutex to protect the log file
-    defer l.mu.Unlock()                 // Unlock mtx after writing
-    switch level {                      // Set the symbol based on the log level
-    case Debug:                         // Debug level?
-      l.Symbol = "[DEBUG] "             // Set symbol to [DEBUG]
-    case Info:                          // Info level?
-      l.Symbol = ""                     // Set symbol to none
-    case Warning:                       // Warning level?
-      l.Symbol = "* "                   // Set symbol to *
-    case Error:                         // Error level?
-      l.Symbol = "! "                   // Set symbol to !
-    case Fatal:                         // Fatal level?
-      l.Symbol = "@ "                  // Set symbol to !!
-    }                                   // Done setting the symbol
-    // -------------------------------- //
-    // Get the file size to check if it exceeds 500KiB,
-    // if so, clear the log file.
-    // -------------------------------- //
-    flogInfo, err := os.Stat(logpathname)// Get the file info
-    if err != nil {                     // Error getting file info?
-      fmt.Printf("Failed to get file info: %v\n", err)// Error getting file info
-      return                            // Return if error
-    }                                   // Otherwise, continue.
-    ferrInfo, ers := os.Stat(errpathname)// Get the error file info
-    if ers != nil {                     // Error getting file info?
-      fmt.Printf("Failed to get file info: %v\n", ers)// Error getting file info
-      return                            // Return if error
-    }                                   // Otherwise, continue.
-    flogSiz:=flogInfo.Size()            // Get the file size
-    if flogSiz > maxLogSize {           // If file size exceeds 30KB
-      l.clearLogFile(logpathname)       // Clear the log file
-    }                                   // Otherwise, continue.
-    ferrSiz:=ferrInfo.Size()            // Get the error file size
-    if ferrSiz > maxLogSize {           // If file size exceeds 30KB
-      l.clearLogFile(errpathname)       // Clear the error file
-    }                                   // Otherwise, continue.
-  // ---------------------------------- //
-  // If the message in the buffer is a multiline message we will purge that
-  // buffer and set a recursive entrypoint so that it enters the log
-  // with a clean and nice buffer. 
-  // ---------------------------------- //
-  if strings.Contains(msg,"\n"){        // Does the buffer container a newline?
-    for _,line:=range strings.Split(msg,"\n"){ // Yes split them by line & purge.
-      if line!=""{                      // Is the purged message not empty?
-        l.logMessage(level,line)        // Log that message without the newline.
-      } else {                          // Otherwise...
-        continue                        // Skip the empty line.
-      }                                 // Done checking the line.
-    }                                   // Done splitting the message.
-    return                              // Return if we had to purge a message.
-  }                                     // Otherwise no newline so just fall through.
-  // ---------------------------------- //
-	// Write the log message to the file
-	// ---------------------------------- //
-  maxcol:=168                           // Maximum column size of the log message.
-  if level < Error {
-    str := fmt.Sprintf("%s: %s: %s: %s%s\n", time.Now().Format(time.RFC3339), getAppname(), getFuncName(), l.Symbol, msg)
-    runes:= []rune(str)                 // Convert the string to a slice of runes
-    strlen := len(runes)                // Get the length of the string
-    if strlen > maxcol {                // Msg is longer than 168 characters?
-        // ---------------------------- //
-        // Add the amount of chars of the msg header in whitespaces 
-        // to the beginning of the string.
-        // ---------------------------- // 
-      space := len(time.Now().Format(time.RFC3339)) + len(getAppname()) + len(getFuncName()) + len(l.Symbol) + 4
-      // Create a string with the desired amount of whitespaces
-      spaces := make([]rune, space)     // Create a slice of runes
-      for i := range spaces {           // Iterate over the slice
-        spaces[i] = ' '                 // Fill the slice with spaces
-      }                                 // Done filling the slice
-      spacestr := string(spaces)        // Convert the slice to a string
-      for len(str) > maxcol {           // for a msg larger than 168 characters
-        chunk := str[:maxcol] + "\n"    // Get the first 168 characters
-        l.writeToFile(logpathname, chunk)// Write to the log file
-        str = spacestr + str[maxcol:]   // Add spaces and remainder of str.
-      }                                 // Done formatting the string.
-      l.writeToFile(logpathname, str)   // Write to the log file
-    } else {                            // Else, msg is less than 168 runes.
-        l.writeToFile(logpathname, str) // Write to the log file
-    }                                   // Done writing the log message
-  } else {
-      str := fmt.Sprintf("%s: %s: %s: %s%s\n", time.Now().Format(time.RFC3339), getAppname(), getFuncName(), l.Symbol, msg)
-      runes:=[]rune(str)                // Convert the string to a slice of runes
-      strlen := len(runes)              // Get the length of the string
-      if strlen > maxcol {              // Msg is longer than 168 characters?
-        // ---------------------------- //
-        // Add the amount of chars of the msg header in whitespaces 
-        // to the beginning of the string.
-        // --------------------------- // 
-        space := len(time.Now().Format(time.RFC3339)) + len(getAppname()) + len(getFuncName()) + len(l.Symbol) + 4
-        spaces := make([]rune, space) // Create a slice of runes
-        for i := range spaces {       // Iterate over the slice
-          spaces[i] = ' '             // Fill the slice with spaces
-        }                             // Done filling the slice
-        spacestr := string(spaces)    // Convert the slice to a string
-        for len(str) > maxcol {       // for a msg larger than 168 characters
-          chunk := str[:maxcol] + "\n"// Get the first 168 characters
-          l.writeToFile(logpathname, chunk) // Write to the log file
-          l.writeToFile(errpathname, chunk) // Write to the error file
-          str = spacestr + str[maxcol:]  // Add spaces and remainder of str.
-        }                               // Done formatting the string.
-        str = "\n" + fmt.Sprintf("%s (line %d)", spacestr, getLineNumber())
-        l.writeToFile(logpathname, str) // Write to the log file
-        l.writeToFile(errpathname, str) // Write to the error file
-      } else {                          // Else, msg is less than 168 characters
-          l.writeToFile(logpathname, str)// Write to the log file
-          l.writeToFile(errpathname, str) // Write to the error file
-      }                                 // Done splitting the message.
-    }                                   // Done checking the length of the message
-}                                       // ---------logMessage-------- //
-*/
 // Deb logs a debug message
 func (l *Logger) Deb(format string, args ...interface{}) bool {
     msg := fmt.Sprintf(format, args...)
