7ad3bfd767ad64a42beca19b38fcfb3a87309561
J. Enrique Peraza, BScEE MEngEE <141517235+perazaharmonics@users.noreply.github.com>
2025-05-17 17:11:56 -0400

    Update sys_pipe_linux_amd64.go

--- pipe/sys_pipe_linux_amd64.go
+++ pipe/sys_pipe_linux_amd64.go
@@ -6,6 +6,7 @@
 package pipe
 
 import (
+	"os"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
@@ -20,6 +21,9 @@ const (
 	F_SETPIPE_SZ = unix.F_SETPIPE_SZ
 	// Re-export the ioctl request flag for FIONREAD:
 	FIONREAD = 0x541B // FIONREAD/TIOCINQ request flag value.
+	// Popen read and write flags:
+	POPENREAD=0      
+	POPENWRITE=1
 )
 
 // Pipe is a wrapper around the pipe(2) syscall.
@@ -135,3 +139,69 @@ func Dup3(oldfd, newfd, flags int) (int, error) {
   }                                     // No, return the new fd and nil.
   return int(r),nil                     // Return the new fd and nil.
 }
+// Popen is similar to C's popen("cmd",mode). It creates a pipe
+// then forks. In the child it hooks up pipe -> stdin/stdout, then 
+// execve("/bin/sh","-c",cmd). In the parent it closes the unused end
+// of the pipe. Flags must be 0, POPENREAD or POPENWRITE.
+func Popen(cmd string, flags int) (fd, pid int, err error) {
+  // ---------------------------------- //
+  // First create a pipe
+  // ---------------------------------- //
+  var fds [2]int32                      // Our file descriptor set.
+  if _,_,e:=unix.Syscall(unix.SYS_PIPE2,uintptr(unsafe.Pointer(&fds)),0,0);e!=0{
+    return 0,0,e                        // Pipe creation failed.
+  }                                     // Pipe created.
+  // ---------------------------------- //
+  // Fork the process
+  // ---------------------------------- //
+  pidraw,_,errno:=unix.Syscall(unix.SYS_FORK,0,0,0)
+  if errno!=0{                          // Fork failed?
+	unix.Close(int(fds[0]))             // Yes, close the pipe.
+	unix.Close(int(fds[1]))             // Close the other end.
+	return 0,0,errno                    // Yes, return 0 and error.
+  }                                     // Done checking error
+  pid=int(pidraw)                       // Get the pid
+  if pid==0{                            // Are we the child process.
+    if flags==POPENREAD{                // Yes, we are the child and we writing.
+	  // ------------------------------ //
+	  // Child writes into pipe -> Dup2(fds[1],STDOUT_FILENO)
+	  // ------------------------------ //
+      unix.Close(int(fds[0]))           // Close the read end of the pipe.
+	  unix.Dup2(int(fds[1]),int(unix.Stdout)) // Redirect stdout to pipe.
+	} else{                             // We are the child and we reading.
+      // ------------------------------ //
+	  // Child reads from pipe -> Dup2(fds[0],STDIN_FILENO)
+	  // ------------------------------ //
+	  unix.Close(int(fds[1]))           // Close the write end of the pipe.
+	  unix.Dup2(int(fds[0]),int(unix.Stdin)) // Redirect stdin to pipe.
+	}                                   // Done acting according to pid.
+	arg0 := "sh"                        // Get the first argument.
+	arg1 := "-c"                        // Get the second argument.
+	argv := []string{arg0, arg1, cmd}   // Create the argument list as []string.
+	env := os.Environ()                 // Get the environment as []string.
+	// -------------------------------- //
+	// Now execve the command.
+	// -------------------------------- //
+	unix.Exec("/bin/sh", argv, env)     // Execute the command with []string env.
+	return -1,pid,unix.EINVAL              // Execve failed, return 0 and error.
+  }                                     // Done checking pid.
+  // ---------------------------------- //
+  // Parent process
+  // ---------------------------------- //
+  if flags==POPENREAD{                  // We are the parent and we reading.
+	unix.Close(int(fds[1]))             // Close the write end of the pipe.
+	return int(fds[0]),pid,nil          // Return the read end of the pipe.
+  }                                     // Done checking if reading.
+  unix.Close(int(fds[0]))               // Close the read end of the pipe.
+  return int(fds[1]),pid,nil            // Return the write end of the pipe.
+}                                       // ------------ Popen ------------
+
+// PClose waits for child pid to exit and returns its exit status.
+func Pclose(pid int) (int,error){
+  var ws unix.WaitStatus                // Create a wait status variable.
+  _,err:=unix.Wait4(pid,&ws,0,nil)      // Wait for the child to exit.
+  if err!=nil{                          // Wait failed?
+    return -1,err                       // Yes, return -1 and error.
+  }                                     // Done checking error.
+  return ws.ExitStatus(),nil            // Return the exit status and nil.
+}                                       // ----------- Pclose -----------
