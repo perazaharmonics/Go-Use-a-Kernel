52754c81b934a5cdfd06a352e830cc6b2d9f6972
J. Enrique Peraza, BScEE MEngEE <141517235+perazaharmonics@users.noreply.github.com>
2025-06-03 06:19:24 -0400

    More robust

--- configuration/api.go
+++ configuration/api.go
@@ -15,7 +15,9 @@
 package configuration
 import (
 		"io"
-	  "github.com/perazaharmonics/gosys/internal/logger"
+		"time"
+		"golang.org/x/sys/unix"
+	  "github.com/ljt/ProxyServer/internal/logger"
 
 )
 
@@ -48,15 +50,88 @@ type ParameterAPI interface{
   NewParameter(name, valuestr string,comments *Comment, imported bool) *Parameter
 	CopyParameter(*Parameter) *Parameter
 	IsImported() bool
+
+  // Set a value for this parameter.
 	SetValue(valuestr string, quote byte) error
 	SetValuePtr(value string,quote byte) error
 	SetValuePtrOnIndex(i uint,value string,quote byte) error
-	GetNValues() uint
+
+	// Get a CSV list of values for this parameter.
 	GetValueArray() []string
+	// Get a value for this parameter.
 	GetValue(i uint) string
-  GetValues() string
-	GetValueBool(i uint,tval string, fval string) (result bool, err error)
-	ScanValue(i int, fmt string, dest any) error
+	// Character values
+	GetValueByte(value string, dest *byte) error
+	GetValueByteByIndex(i uint,dest *byte) error
+
+	// Times and durations
+	GetValueTimespec(value string, dest *unix.Timespec) error
+	GetValueTimespecByIndex(i uint,dest *unix.Timespec) error
+	GetValueDuration(value string, dest *time.Duration) error
+	GetValueDurationByIndex(i uint,dest *time.Duration) error
+
+	// Time since epoch
+	GetValueTime(value string, dest *time.Time) error
+	GetValueTimeByIndex(i uint,dest *time.Time) error
+
+	// Signed Integers
+	GetValueInt(value string, dest *int) error
+	GetValueIntByIndex(i uint,dest *int) error
+	GetValueInt8(value string, dest *int8) error
+	GetValueInt8ByIndex(i uint,dest *int8) error
+	GetValueInt16(value string, dest *int16) error
+	GetValueInt16ByIndex(i uint,dest *int16) error
+	GetValueInt32(value string, dest *int32) error
+	GetValueInt32ByIndex(i uint,dest *int32) error
+	GetValueInt64(value string, dest *int64) error
+	GetValueInt64ByIndex(i uint,dest *int64) error
+
+	// Unicode, binary and hex values
+	GetValueRune(value string, dest *rune) error
+	GetValueRuneByIndex(i uint,dest *rune) error
+	GetValueBinary(value string, dest *string) error
+	GetValueHex(value string, dest *string) error
+	GetValueOctal(value string, dest *string) error
+
+	// Unsigned integers
+  GetValueUint(value string, dest *uint) error
+	GetValueUintByIndex(i uint,dest *uint) error
+	GetValueUint8(value string, dest *uint8) error
+	GetValueUint8ByIndex(i uint,dest *uint8) error
+	GetValueUint16(value string, dest *uint16) error
+	GetValueUint16ByIndex(i uint, dest *uint16) error
+	GetValueUint32(value string, dest *uint32) error
+	GetValueUint32ByIndex(i uint,dest *uint32) error
+	GetValueUint64(value string, dest *uint64) error
+	GetValueUint64ByIndex(i uint,dest *uint64) error
+
+	// Floating point values
+	GetValueFloat32(value string, dest *float32) error
+	GetValueFloat32ByIndex(i uint,dest *float32) error
+	GetValueFloat64(value string,dest *float64) error
+	GetValueFloat64ByIndex(i uint,dest *float64) error
+
+	// Floating point values with precision
+	GetValuePrecisionFloat32(value,precision string,dest *float32) error
+	GetValuePrecisionFloat32ByIndex(i uint,precision string,dest *float32) error
+	GetValuePrecisionFloat64(value,precision string,dest *float64) error
+	GetValuePrecisionFloat64ByIndex(i uint,precision string,dest *float64) error
+
+	// Complex numbers
+	GetValueComplex64(value string,dest *complex64) error
+	GetValueComplex64ByIndex(i uint,dest *complex64) error
+	GetValueComplex128(value string,dest *complex128) error
+	GetValueComplex128ByIndex(i uint,dest *complex128) error
+
+	// Scientific notation
+	GetValueSI(value string,dest *string) error
+  // Get a boolean value for a parameter name
+	GetValueBool(i uint,tval string, fval string,dest bool) error
+	scanValue(format string, dest any) error
+	scanValueByIndex(i int, fmt string, dest any) error
+
+	GetNValues() uint
+  GetValues() string	
 	GetName() string
 	GetNext() *Parameter
 	SetNext(p *Parameter)
@@ -98,13 +173,7 @@ type SectionAPI interface{
 	AppendParameter(name string, valuestr string, comments *Comment,imported bool) *Parameter
 	// Add a section to this section.
 	AppendSection(name string, imported bool)
-	// Set a parameter's value
-	SetValue(name string,value string,quote byte) error
-	// Set a parameter's value to the given pointer.
-	SetValuePtr(name string,value string,quote byte) error
-	// Set a parameter's value to the given pointer on the given index.
-	SetValuePtrOnIndex(name, value string, i uint, quote byte) error
-	SetValueInFormat(name string,i int,format string,src any) error
+
 	GetName() string
 	// Get the first value for any array parameter.
 	FindFirstParameter() *Parameter       // Get pointer to first parameter
@@ -126,6 +195,16 @@ type SectionAPI interface{
 	SelectFirstParameter()                // Select the first parameter.
 	SelectParameter(p *Parameter)         // Select the given parameter.
 	SelectParameterByName(name string) error// Select the parameter by name.
+	
+		// Set a parameter's value
+	SetValue(name string,value string,quote byte) error
+	// Set a parameter's value to the given pointer.
+	SetValuePtr(name string,value string,quote byte) error
+	// Set a parameter's value to the given pointer on the given index.
+	SetValuePtrOnIndex(name, value string, i uint, quote byte) error
+	SetValueInFormat(name string,i int,format string,src any) error
+	
+	
 	GetParameter(name string, searchParents bool) *Parameter // Get a parameter by name.
 	// Get array of values for a parameter name.
 	GetValueArray(name string) []string
@@ -134,8 +213,73 @@ type SectionAPI interface{
 	GetValues(name string) string           // Get values for a parameter name.
   // Boolean
 	GetValueBool(name string,i uint,tval string, fval string) (bool,error)// Get a boolean value for a parameter name.
-	// Integer
-	ScanValue(name string,i int, format string, dest any) error
+	// Byte values (character values)
+	GetValueByte(name string, dest *byte) error
+	GetValueByteByIndex(name string,i uint,dest *byte) error
+
+	// Times and durations
+	GetValueTimespec(name string, dest *unix.Timespec) error
+	GetValueTimespecByIndex(name string,i uint,dest *unix.Timespec) error
+	GetValueDuration(name string, dest *time.Duration) error
+	GetValueDurationByIndex(name string,i uint,dest *time.Duration) error
+
+	// Time since epoch
+	GetValueTime(name string, dest *time.Time) error
+	GetValueTimeByIndex(name string, i uint,dest *time.Time) error
+
+	// Signed Integers
+	GetValueInt(name string, dest *int) error
+	GetValueIntByIndex(name string,i uint,dest *int) error
+	GetValueInt8(name string, dest *int8) error
+	GetValueInt8ByIndex(name string,i uint,dest *int8) error
+	GetValueInt16(name string, dest *int16) error
+	GetValueInt16ByIndex(name string,i uint,dest *int16) error
+	GetValueInt32(name string, dest *int32) error
+	GetValueInt32ByIndex(name string,i uint,dest *int32) error
+	GetValueInt64(name string, dest *int64) error
+	GetValueInt64ByIndex(name string,i uint,dest *int64) error
+
+	// Unicode, binary and hex values
+	GetValueRune(name string, dest *rune) error
+	GetValueRuneByIndex(name string,i uint,dest *rune) error
+	GetValueBinary(name string, dest *string) error
+	GetValueHex(name string, dest *string) error
+	GetValueOctal(name string, dest *string) error
+
+	// Unsigned integers
+  GetValueUint(name string, dest *uint) error
+	GetValueUintByIndex(name string,i uint,dest *uint) error
+	GetValueUint8(name string, dest *uint8) error
+	GetValueUint8ByIndex(name string,i uint,dest *uint8) error
+	GetValueUint16(name string, dest *uint16) error
+	GetValueUint16ByIndex(name string,i uint, dest *uint16) error
+	GetValueUint32(name string, dest *uint32) error
+	GetValueUint32ByIndex(name string,i uint,dest *uint32) error
+	GetValueUint64(name string, dest *uint64) error
+	GetValueUint64ByIndex(name string,i uint,dest *uint64) error
+
+	// Floating point values
+	GetValueFloat32(name string, dest *float32) error
+	GetValueFloat32ByIndex(name string,i uint,dest *float32) error
+	GetValueFloat64(name string,dest *float64) error
+	GetValueFloat64ByIndex(name string,i uint,dest *float64) error
+
+	// Floating point values with precision
+	GetValuePrecisionFloat32(name,precision string,dest *float32) error
+	GetValuePrecisionFloat32ByIndex(name string,i uint,precision string,dest *float32) error
+	GetValuePrecisionFloat64(name string,value,precision string,dest *float64) error
+	GetValuePrecisionFloat64ByIndex(name string,i uint,precision string,dest *float64) error
+
+	// Complex numbers
+	GetValueComplex64(name string,dest *complex64) error
+	GetValueComplex64ByIndex(name string,i uint,dest *complex64) error
+	GetValueComplex128(name string,dest *complex128) error
+	GetValueComplex128ByIndex(name string,i uint,dest *complex128) error
+
+	// Scientific notation
+	GetValueSI(name string,dest *string) error	
+	scanValue(value,format string, dest any) error
+	scanValueByIndex(name string,i int, format string, dest any) error
   ClearParameters() error                // Clear all parameters in this section.
 	SetParentNames(name string)            // First pass.
 	SetParentSection(i uint, p *Section)   // Second pass.
@@ -222,6 +366,112 @@ type ConfigurationAPI interface{
 	GetValue(name string) string       // Get a string parameter from the selected section.
 	GetValues(name string) string      // Get source string for parameter.
 	GetValueByIndex(name string, i uint) string // Get a value by index for a parameter.
+	
+	// Byte values (character values)
+	GetValueByte(name string, dest *byte) error
+	SetValueByte(name string, value byte) error
+	GetValueByteByIndex(name string,i uint,dest *byte) error
+	SetValueByteByIndex(name string, i uint, value byte) error
+
+	// Times and durations
+	GetValueTimespec(name string, dest *unix.Timespec) error
+	GetValueTimespecByIndex(name string,i uint,dest *unix.Timespec) error
+	GetValueDuration(name string, dest *time.Duration) error
+	GetValueDurationByIndex(name string,i uint,dest *time.Duration) error
+	// Time since epoch
+	GetValueTime(name string, dest *time.Time) error
+	GetValueTimeByIndex(name string, i uint,dest *time.Time) error
+
+	// Signed Integers
+	GetValueInt(name string, dest *int) error
+	SetValueInt(name string, value int) error
+	GetValueIntByIndex(name string,i uint,dest *int) error
+	SetValueIntByIndex(name string, i uint, value int) error
+	GetValueInt8(name string, dest *int8) error
+	SetValueInt8(name string, value int8) error
+	GetValueInt8ByIndex(name string,i uint,dest *int8) error
+	SetValueInt8ByIndex(name string, i uint, value int8) error
+	GetValueInt16(name string, dest *int16) error
+	SetValueInt16(name string, value int16) error
+	GetValueInt16ByIndex(name string,i uint,dest *int16) error
+	SetValueInt16ByIndex(name string, i uint, value int16) error
+	GetValueInt32(name string, dest *int32) error
+	SetValueInt32(name string, value int32) error
+	GetValueInt32ByIndex(name string,i uint,dest *int32) error
+	SetValueInt32ByIndex(name string, i uint, value int32) error
+	GetValueInt64(name string, dest *int64) error
+	SetValueInt64(name string, value int64) error
+	GetValueInt64ByIndex(name string,i uint,dest *int64) error
+	SetValueInt64ByIndex(name string, i uint, value int64) error
+
+	// Unicode, binary and hex values
+	GetValueRune(name string, dest *rune) error
+	SetValueRune(name string, value rune) error
+	GetValueRuneByIndex(name string,i uint,dest *rune) error
+	SetValueRuneByIndex(name string, i uint, value rune) error
+	GetValueBinary(name string, dest *string) error
+	SetValueBinary(name string, value string) error
+	GetValueHex(name string, dest *string) error
+	SetValueHex(name string, value string) error
+	GetValueOctal(name string, dest *string) error
+	SetValueOctal(name string, value string) error
+
+	// Unsigned integers
+  GetValueUint(name string, dest *uint) error
+	SetValueUint(name string, value uint) error
+	GetValueUintByIndex(name string,i uint,dest *uint) error
+	SetValueUintByIndex(name string, i uint, value uint) error
+	GetValueUint8(name string, dest *uint8) error
+	SetValueUint8(name string, value uint8) error
+	GetValueUint8ByIndex(name string,i uint,dest *uint8) error
+	SetValueUint8ByIndex(name string, i uint, value uint8) error
+	GetValueUint16(name string, dest *uint16) error
+	SetValueUint16(name string, value uint16) error
+	GetValueUint16ByIndex(name string,i uint, dest *uint16) error
+	SetValueUint16ByIndex(name string, i uint, value uint16) error
+	GetValueUint32(name string, dest *uint32) error
+	SetValueUint32(name string, value uint32) error
+	GetValueUint32ByIndex(name string,i uint,dest *uint32) error
+	SetValueUint32ByIndex(name string, i uint, value uint32) error
+	GetValueUint64(name string, dest *uint64) error
+	SetValueUint64(name string, value uint64) error
+	GetValueUint64ByIndex(name string,i uint,dest *uint64) error
+	SetValueUint64ByIndex(name string, i uint, value uint64) error
+
+	// Floating point values
+	GetValueFloat32(name string, dest *float32) error
+	SetValueFloat32(name string, value float32) error
+	GetValueFloat32ByIndex(name string,i uint,dest *float32) error
+	SetValueFloat32ByIndex(name string, i uint, value float32) error
+	GetValueFloat64(name string,dest *float64) error
+	SetValueFloat64(name string, value float64) error
+	GetValueFloat64ByIndex(name string,i uint,dest *float64) error
+	SetValueFloat64ByIndex(name string, i uint, value float64) error
+
+	// Floating point values with precision
+	GetValuePrecisionFloat32(name,precision string,dest *float32) error
+	SetValuePrecisionFloat32(name,precision string,value float32) error
+	GetValuePrecisionFloat32ByIndex(name string,i uint,precision string,dest *float32) error
+	SetValuePrecisionFloat32ByIndex(name string, i uint, precision string, value float32) error
+	GetValuePrecisionFloat64(name string,value,precision string,dest *float64) error
+	SetValuePrecisionFloat64(name string,precision string,value float64) error
+	GetValuePrecisionFloat64ByIndex(name string,i uint,precision string,dest *float64) error
+	SetValuePrecisionFloat64ByIndex(name string, i uint, precision string, value float64) error
+
+	// Complex numbers
+	GetValueComplex64(name string,dest *complex64) error
+	SetValueComplex64(name string, value complex64) error
+	GetValueComplex64ByIndex(name string,i uint,dest *complex64) error
+	SetValueComplex64ByIndex(name string, i uint, value complex64) error
+	GetValueComplex128(name string,dest *complex128) error
+	SetValueComplex128(name string, value complex128) error
+	GetValueComplex128ByIndex(name string,i uint,dest *complex128) error
+	SetValueComplex128ByIndex(name string, i uint, value complex128) error
+
+	// Scientific notation
+	GetValueSI(name string,dest *string) error
+	SetValueSI(name string, value string) error	
+	
 	GetValueBySection(section string, parameter string) string // Get value of named section.
 	GetValueBySectionAndIndex(section, name string, i uint) string
 	GetValueBool(parameter string, value *bool) error // Get a boolean parameter from the selected section.
